4efccb489aa7096dd3ce1c2dddebfc38d75a3bb8
RoseGabriely <rosegabriely14@gmail.com>
2022-09-10 17:16:51 -0400

    Adds title and header

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-
+      <body>
+        <section class="title">Rose's Portfolio</section>
+        <header>
+          <h1>Rose Gabriely</h1>
+          <nav>
+            <a href="index.html">Home</a>
+            <a href="about.html">About</a>
+            <a href="contact.html">Contact</a>
+            <a href="projects.html">Projects</a>
+          </nav>
+        </header>
+      </body>
     </div>
   );
 }
