98cda2a0039c43ee951ec17e21ee35e73c12d692
RoseGabriely <rosegabriely14@gmail.com>
2022-09-12 19:16:46 -0400

    Adds about component with styling

---
+++ src/About.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./About.css";
+
+function About() {
+  return (
+    <div className="About">
+      <h2 className="name">About</h2>
+      <p className="journey">
+        A portfolio of solo and collaborative projects worked on with various
+        teams. Demonstrate front-end, back-end and full-stack work in React and
+        Node while also incorporating other libraries and frameworks.
+      </p>
+    </div>
+  );
+}
+
+export default About;
