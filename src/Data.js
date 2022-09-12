88189d194082bab4089c9b731b3ce38c8a237920
RoseGabriely <rosegabriely14@gmail.com>
2022-09-12 18:59:56 -0400

    Creates separate file for project data

---
+++ src/Data.js
@@ -0,0 +1,32 @@
+const projArr = [
+  {
+    key: 1,
+    image: "https://i.ibb.co/whjN8Ks/download.png",
+    link: "https://github.com/RoseGabriely/unit4buildweek",
+    caption:
+      "An API for an African Marketplace website where business owners could list products at various market locations",
+  },
+  {
+    key: 2,
+    image: "https://i.ibb.co/9Tr6Vt1/download.png",
+    link: "https://github.com/RoseGabriely/coder-heroes-fe",
+    caption:
+      "Front-end of coderheroes, a booking website for parents to sign their young kids up for coding courses.",
+  },
+  {
+    key: 3,
+    image: "https://i.ibb.co/9Tr6Vt1/download.png",
+    link: "https://github.com/RoseGabriely/coder-heroes-be",
+    caption:
+      "Back-end of coderheroes, a booking website for parents to sign their young kids up for coding courses.",
+  },
+  {
+    key: 4,
+    image: "https://i.ibb.co/7VRF1wX/download.png",
+    link: "https://github.com/Build-Week-Water-My-Plantz/front-end",
+    caption:
+      "Front-end work on a website to keep track of plants, their information, and their watering schedules.",
+  },
+];
+
+export default projArr;
