d544e9e4aa8ee9f78409deccbf70c90b455b568f
RoseGabriely <rosegabriely14@gmail.com>
2022-09-12 19:27:16 -0400

    Adds contact component with styling

---
+++ src/Contact.js
@@ -0,0 +1,19 @@
+import React from "react";
+import "./Contact.css";
+
+function Contact() {
+  return (
+    <div className="Contact">
+      <p>Email: rosegabriely14@gmail.com</p>
+      <p>Phone Number: 857-707-0062</p>
+      <p>
+        Linkedin:{" "}
+        <a href="https://www.linkedin.com/in/rosegabriely/">
+          www.linkedin.com/in/rosegabriely/
+        </a>
+      </p>
+    </div>
+  );
+}
+
+export default Contact;
