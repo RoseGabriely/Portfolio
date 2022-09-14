a1a20d9183724b313f27df0242e081365bc9bf38
RoseGabriely <rosegabriely14@gmail.com>
2022-09-13 20:24:30 -0400

    Adds github to contact section

--- src/Contact.js
+++ src/Contact.js
@@ -8,8 +8,22 @@ function Contact() {
       <p>Phone Number: 857-707-0062</p>
       <p>
         Linkedin:{" "}
-        <a href="https://www.linkedin.com/in/rosegabriely/">
+        <a
+          href="https://www.linkedin.com/in/rosegabriely/"
+          target="_blank"
+          rel="noreferrer"
+        >
           www.linkedin.com/in/rosegabriely/
+        </a>
+      </p>
+      <p>
+        Github:{" "}
+        <a
+          href="https://github.com/RoseGabriely"
+          target="_blank"
+          rel="noreferrer"
+        >
+          https://github.com/RoseGabriely
         </a>
       </p>
     </div>
