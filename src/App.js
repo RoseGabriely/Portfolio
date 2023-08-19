8ead0f03d559e8cc785ac6792a297eb6a47b5162
RoseGabriely <rosegabriely14@gmail.com>
2023-08-19 06:42:53 -0400

    Adds animate presence & updates info

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import "./App.css";
 import React from "react";
 import { Routes, Route, Link } from "react-router-dom";
+import { AnimatePresence } from "framer-motion";
 import Home from "./Home";
 import About from "./About";
 import Contact from "./Contact";
@@ -14,53 +15,55 @@ import { FaGithub } from "react-icons/fa";
 function App() {
   return (
     <div className="App">
-      <section className="title">Rose's Portfolio</section>
       <header>
-        <h1>Rose Gabriely</h1>
+        <div className="title">
+          <h1>Rose Gabriely</h1>
+          <h2>Full Stack Web Developer</h2>
+        </div>
         <nav>
           <Link to="/">Home</Link>
           <Link to="/about">About</Link>
-          <Link to="/contact">Contact</Link>
+          <Link to="/contact">Resume</Link>
           <Link to="/projects">Projects</Link>
         </nav>
       </header>
-      <section className="page">
+      <AnimatePresence mode="wait">
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="about" element={<About />} />
-          <Route path="contact" element={<Contact />} />
-          <Route path="projects" element={<Projects />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/projects" element={<Projects />} />
         </Routes>
-        <footer>
-          <p className="question">Contact me!</p>
-          <div className="contact">
-            <a
-              href="mailto: rosegabriely14@gmail.com"
-              target="_blank"
-              rel="noreferrer"
-            >
-              <FaEnvelope />
-            </a>
-            <a href="tel:8577070062">
-              <FaPhone />
-            </a>
-            <a
-              href="https://www.linkedin.com/in/rosegabriely/"
-              target="_blank"
-              rel="noreferrer"
-            >
-              <FaLinkedin />
-            </a>
-            <a
-              href="https://github.com/RoseGabriely"
-              target="_blank"
-              rel="noreferrer"
-            >
-              <FaGithub />
-            </a>
-          </div>
-        </footer>
-      </section>
+      </AnimatePresence>
+      <footer>
+        <p className="connect">Contact Me</p>
+        <div className="contact">
+          <a
+            href="mailto: rosegabriely14@gmail.com"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <FaEnvelope />
+          </a>
+          <a href="tel:8577070062">
+            <FaPhone />
+          </a>
+          <a
+            href="https://www.linkedin.com/in/rosegabriely/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <FaLinkedin />
+          </a>
+          <a
+            href="https://github.com/RoseGabriely"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <FaGithub />
+          </a>
+        </div>
+      </footer>
     </div>
   );
 }
